5e1b8bf778c9125af4626dddf8eda3941f824366
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-25 16:07:42 -0600

    calling sequelize from config and using Sequelize Store to store session data

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// calling path, express, session, express-handlebars, routes, helpers
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+// import sequelize connection
+const sequelize = require('./config/connection');
+// import sequelize store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
