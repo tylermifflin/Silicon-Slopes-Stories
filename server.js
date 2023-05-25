fcfd029b8f9a437bdfc2a8e62d16efa4c1d5afac
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-25 16:08:57 -0600

    calling express and port

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+
 // import sequelize connection
 const sequelize = require('./config/connection');
+
 // import sequelize store
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// calling express
+const app = express();
+
+// calling port
+const PORT = process.env.PORT || 3001;
