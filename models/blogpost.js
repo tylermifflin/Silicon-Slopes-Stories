45679d5f85870857891c50e2db5389117bfba81d
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 20:28:18 -0600

    set up id and title column for blogpost table

--- models/blogpost.js
+++ models/blogpost.js
@@ -4,3 +4,15 @@ const sequelize = require('../config/connection');
 class BlogPost extends Model {}
 
 BlogPost.init(
+    {
+        id : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        
