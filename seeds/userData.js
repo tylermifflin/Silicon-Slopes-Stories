afca1c9d0427e805267cca8c4767b1411e97acbd
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 17:17:26 -0600

    used user model for userData.js to seed data and exported to seed.js

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,17 +1,26 @@
-[
+// seed database with userData moedl
+
+const { User } = require('../models');
+
+const userData = [
     {
         "name": "Rick James",
         "email": "rjames@test.com",
-        "password": "rickjames1"
+        "password": "rickjames1",
     },
     {
         "name": "John Doe",
         "email": "jdoe@test.com",
-        "password": "johndoe123"
+        "password": "johndoe123",
     },
     {
         "name": "Billy Bob",
         "email": "billybob@test.com",
-        "password": "billybob12"
-    }
-]
+        "password": "billybob12",
+    },
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
+
