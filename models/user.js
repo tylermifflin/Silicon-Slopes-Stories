b3a7412ad8ac8637a6b2a4aa48de8f5ff873e19b
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 20:07:18 -0600

    using bcrypt to check password and setting up User id column

--- models/user.js
+++ models/user.js
@@ -2,3 +2,17 @@ const {Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
