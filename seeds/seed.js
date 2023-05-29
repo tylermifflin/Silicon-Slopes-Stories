7b450d848f131904ca0bb3c5663e481b7fb0ee77
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 17:03:27 -0600

    using the user and blogpost seed files and seedDatabase function

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,8 @@
 // setting up seed data to import into the database from blogpostData.json and userData.json
 const sequelize = require('../config/connection');
 const { User, BlogPost } = require('../models');
+
+const userData = require('./userData.json');
+const blogpostData = require('./blogpostData.json');
+
+const seedDatabase = async () => {
