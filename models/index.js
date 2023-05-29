ae2ab5de37e931b769ad8c941ca61daef95464eb
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 20:45:59 -0600

    fixed to typos in my require for user and blogpost

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const BlogPost = require('./BlogPost');
+const User = require('./user');
+const BlogPost = require('./blogpost');
 
 User.hasMany(BlogPost, {
     foreignKey: 'user_id',
