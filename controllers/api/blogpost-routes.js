73d42741dfcf0b5a0dd9307f8f77995d7bd06ed0
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 15:55:36 -0600

    set up router.post for api/blogposts using withAuth and await Blogpost.create if user is logged in then they can post a newBlogpost and catch error if any

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -2,3 +2,15 @@ const router = require('express').Router();
 const { Blogpost } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// POST /api/blogposts
+router.post ('/', withAuth, async (req, res) => {
+    try {
+        const newBlogpost = await Blogpost.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newBlogpost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
