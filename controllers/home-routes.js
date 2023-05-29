5c36434489fcb9fec7513d88da40ab75c49180ff
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 20:56:50 -0600

    set up router.get to get all blog posts using findall and include User and attributes name

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,14 @@ const router = require('express').Router();
 const { User, BlogPost } = require('../models');
 const withAuth = require('../utils/auth');
 
+// GET all blog posts for dashboard
+router.get('/', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
