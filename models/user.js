eea2f9d481c6093371a67f5e88025e3c4918ebd4
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 20:10:15 -0600

    set up name and email column and used validate to check if it is an email

--- models/user.js
+++ models/user.js
@@ -16,3 +16,15 @@ User.init(
             primaryKey: true,
             autoIncrement: true
         },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
