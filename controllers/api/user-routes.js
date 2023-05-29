018543fd4c0fc5eba2915cd7269af98a767d032c
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 21:49:10 -0600

    if doesn't match email give error with message incorrect email

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,3 +21,9 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
     try { 
         const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email' });
+            return;
+        }
