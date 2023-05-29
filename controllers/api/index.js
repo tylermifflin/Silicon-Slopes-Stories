32e7fc14ddf0c57555fa506d06b3cb8d688b3229
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 16:21:06 -0600

    using router.use for users and blogposts in order to use routes for api routes and exporting to router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,3 +2,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const blogpostRoutes = require('./blogpost-routes');
+
+router.use('/users', userRoutes);
+router.use('/blogposts', blogpostRoutes);
+
+module.exports = router;
