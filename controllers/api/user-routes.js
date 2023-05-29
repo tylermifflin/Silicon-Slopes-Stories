f821fdfcbaa7439fecfd35d46ebda43d078a1307
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-28 21:38:31 -0600

    set up router.post for api/users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,7 @@
+// Purpose: user routes, login, logout, signup, 
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// POST /api/users
+router.post('/', async (req, res) => {
+    try {
