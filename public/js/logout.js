ccd3f03058aa4569999b67e7abd42e714907b240
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 21:52:53 -0600

    using method Post

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,6 @@
 // set up logout function
 const logout = async () => {
     const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
