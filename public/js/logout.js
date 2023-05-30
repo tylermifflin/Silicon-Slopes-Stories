35732b9e535f40333115a201f8a675c940788471
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 21:56:56 -0600

    if logout.ok redirect to homepage else alert logout unsuccessful and added event listener for logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,16 @@
 // set up logout function
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
+    const Logout = await fetch('/api/users/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
+    if (Logout.ok) {
+        // redirect to homepage
+        document.location.replace('/');
+    } else {
+        alert('Logout unsuccessful');
+    }
+};
+
+// add event listener to logout button
+document.querySelector('#logout').addEventListener('click', logout);
