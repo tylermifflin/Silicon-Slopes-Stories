6880652d1f489be0019faade45a702661185d2dc
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-29 18:47:20 -0600

    check if email and password are entered

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,11 @@
 // set up login and signup forms
-const loginFromHandler = async (event) => {
+const loginTemplate = async (event) => {
     event.preventDefault();
 
     // get values from login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
+    // if email and password are entered
+    if (email && password) {
+        // send POST request to login route
