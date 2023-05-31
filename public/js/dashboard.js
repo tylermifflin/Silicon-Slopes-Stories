bc67af7eba8f30c6b2b467f138e758adde132c66
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-31 14:55:04 -0600

    setting up POST route to add new blogpost to dashboard for api/blogposts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,4 +6,10 @@ const newBlogPostHandler = async (event) => {
     const title = document.querySelector('#blog-title').value.trim();
     const content = document.querySelector('#blog-content').value.trim();
 
-    
+    // if both the title and content exist, send a POST request to the API endpoint
+    if (title && content) {
+        const newBlogPost = await fetch('/api/blogposts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
