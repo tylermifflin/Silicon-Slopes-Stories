05118bc85555253d51997200b9e958d3dc6431f1
Tyler Mifflin <you@example.com>
2023-06-07 21:58:33 -0600

    updated seeds to have commentData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 // setting up seed data to import into the database from blogpostData.js and userData.js
 const sequelize = require('../config/connection');
-const { BlogPost, User } = require('../models');
+const { BlogPost, User, Comment } = require('../models');
 
 const blogpostData = require('./blogpostData.json');
 const userData = require('./userData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -19,9 +20,16 @@ const seedDatabase = async () => {
         user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
+
+    for (const comment of commentData) {
+        await Comment.create({
+            ...comment,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+            blogpost_id: blogpostData[Math.floor(Math.random() * blogpostData.length)].id,
+        });
     
     process.exit(0);
-    };
+    }
+};
 
 seedDatabase();
-
